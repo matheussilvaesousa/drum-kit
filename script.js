ca205a18757f5cfabd9aa4fa0a61e2845ec08052
Matheus Sousa <matheussilvaesousa@gmail.com>
2020-09-25 12:14:38 -0300

    Add initial project

---
+++ script.js
@@ -0,0 +1,30 @@
+function play(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+}
+function startAction(e) {
+  const button = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const key = button.querySelector("kbd");
+  const instrument = button.querySelector("span");
+  if (!button) return;
+  button.classList.add("playing-bg");
+  key.classList.add("playing-key");
+  instrument.classList.add("playing-instrument");
+}
+function endAction(e) {
+  const button = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const key = button.querySelector("kbd");
+  const instrument = button.querySelector("span");
+  if (!button) return;
+  button.classList.remove("playing-bg");
+  key.classList.remove("playing-key");
+  instrument.classList.remove("playing-instrument");
+}
+
+window.addEventListener("keydown", (e) => {
+  play(e);
+  startAction(e);
+});
+window.addEventListener("keyup", endAction);
